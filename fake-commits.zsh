8e4128143a11ee7999ea25c088449cd1b18f781f
nickschmitt <nickschmitt@gmail.com>
2021-04-01 00:23:47 -0700

    Script for creating fake commits

---
+++ fake-commits.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+echo "How many days worth of fake git commmits would you like?"
+
+read days
+
+for ((i=$days; i>0; i--))
+do
+	x=$(date -v-${i}d)
+	echo $x
+	git commit --date=${x} --allow-empty --allow-empty-message --no-edit
+done
